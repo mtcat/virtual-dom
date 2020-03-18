ffbf5d4c35be2527188c499f59b8641fa04df159
miaoting <miuting@olymtech.com>
2020-03-18 16:35:17 +0800

    input example

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const tree = dom.el('div', { class: 'wrap' }, [
   dom.el('p', { class: 'top', style: { color: 'red', 'font-size': '22px' } }, '123123123-1'),
   dom.el('p', { class: 'center', style: { color: 'green' } }, '123123123-2'),
   dom.el('p', { class: 'bottom', style: { color: 'blue' } }, '123123123-3'),
+  dom.el('input', { id: 'set-value', value: 'hello world' }),
 ]);
 
 const root = tree.render();
